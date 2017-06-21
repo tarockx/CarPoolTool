71624ad54da8decf2cfd5d42e77482977f96206a
Elia Dal Santo <Elia.DalSanto.GE1418@infocamere.it>
2017-06-21 13:17:15 +0200

    Added active field

--- CarPoolTool/Models/User.cs
+++ CarPoolTool/Models/User.cs
@@ -25,6 +25,7 @@ namespace CarPoolTool.Models
         public string password { get; set; }
         public short admin { get; set; }
         public string display_name { get; set; }
+        public short active { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<CarpoolLog> CarpoolLogs { get; set; }
